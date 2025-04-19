d0e47a1e735be196a12c1fedfb94792680ae7cd7
K.Ashok Kumar <kak.mca@hotmail.com>
2025-04-19 08:46:40 +0530

    Some more learnings from new system

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { noop, Observable } from 'rxjs';
 
 @Component({
     selector: 'about',
@@ -11,9 +12,24 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('click', evt => {
-      console.log(evt)
+    const http$ = new Observable(observer => {
+      fetch('/api/courses')
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        })
     });
-  }
 
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('Completed')
+    );
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { fromEvent, timer } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -8,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {}
+  
+  ngOnInit() {
+
+  }
 }
